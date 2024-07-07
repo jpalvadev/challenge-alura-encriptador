c6a8b3d6678d8bfe937c405798b030f759abc0e8
Juan Pablo Alvarez <jpalvadev@gmail.com>
2024-07-07 19:43:06 -0300

    proyecto terminado

---
+++ README.md
@@ -0,0 +1,18 @@
+# Encriptador-Desencriptador
+
+Este repositorio contiene un encriptador-desencriptador desarrollado como parte del challenge de Oracle ONE. La aplicación permite a los usuarios encriptar y desencriptar mensajes de texto de manera sencilla.
+
+## Descripción del Proyecto
+
+El proyecto sigue parte de la maquetación proporcionada en el challenge, pero se han tomado ciertas libertades en el diseño y el funcionamiento para adaptarlo a un estilo visual propio.
+
+## Uso
+
+1. **Encriptar Texto**: Ingrese el texto que desea encriptar en el área de texto proporcionada y haga clic en el botón "Encriptar".
+2. **Desencriptar Texto**: Ingrese el texto encriptado en el área de texto y haga clic en el botón "Desencriptar".
+3. **Copiar Texto**: Después de encriptar o desencriptar, use el botón "Copiar" para pegar automáticamente el texto en el input del usuario.
+
+## Libertades Tomadas
+
+-   **Maquetación Personalizada**: Se modificaron los aspectos visuales para crear un diseño único.
+-   **Funcionalidad del Botón Copiar**: Se mejoró la funcionalidad del botón copiar para que pegue el texto directamente en el input del usuario, optimizando la experiencia y reduciendo los pasos necesarios.
---
+++ script.js
@@ -0,0 +1,47 @@
+const btnEncriptar = document.getElementById('btn-encriptar');
+const btnDesencriptar = document.getElementById('btn-desencriptar');
+const btnCopiar = document.getElementById('btn-copiar');
+const noMensajeContainer = document.querySelector('.right__no-mensaje');
+
+const userInput = document.querySelector('.middle__textarea');
+const textResult = document.querySelector('.right__text');
+const rightContainer = document.querySelector('.right');
+
+const keys = [
+    { decoded: 'e', coded: 'enter' },
+    { decoded: 'i', coded: 'imes' },
+    { decoded: 'a', coded: 'ai' },
+    { decoded: 'o', coded: 'ober' },
+    { decoded: 'u', coded: 'ufat' },
+];
+
+btnEncriptar.addEventListener('click', () => {
+    if (!userInput.value) return;
+
+    noMensajeContainer.style.display = 'none';
+    btnCopiar.style.display = 'block';
+    textResult.style.display = 'block';
+    rightContainer.classList.add('right--active');
+
+    let str = userInput.value;
+    keys.forEach((key) => {
+        str = str.replaceAll(key.decoded, key.coded);
+    });
+    textResult.textContent = str;
+    userInput.value = '';
+});
+
+btnDesencriptar.addEventListener('click', () => {
+    if (!userInput.value) return;
+    let str = userInput.value;
+    keys.forEach((key) => {
+        str = str.replaceAll(key.coded, key.decoded);
+    });
+    textResult.textContent = str;
+    userInput.value = '';
+});
+
+btnCopiar.addEventListener('click', () => {
+    if (!textResult.textContent) return;
+    userInput.value = textResult.textContent;
+});
